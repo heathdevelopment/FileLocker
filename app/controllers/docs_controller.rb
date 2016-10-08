3783668d28a985e39345355ae77aa1360c22d1c9
heathdevelopment <hgdesign711@gmail.com>
2016-10-08 08:17:20 -0400

    assocation between docs and users

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,6 @@
 class DocsController < ApplicationController
 	before_action :find_doc, only: [:show, :edit, :update, :destroy]
-
+    before_action :authenticate_user!, only: [:new,:create]
 	def index
 		@docs = Doc.all.order("created_at DESC")
 	end
@@ -10,11 +10,11 @@ class DocsController < ApplicationController
 	end
 
 	def new
-		@doc = Doc.new
+		@doc = current_user.docs.build
 	end
 
 	def create
-		@doc = Doc.new(doc_params)
+		@doc = current_user.docs.build(doc_params)
 
 		if @doc.save
 			redirect_to @doc
